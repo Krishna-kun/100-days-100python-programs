c7fa176840b0911bbf028d94e0d0f8b11283ab4d
Krishna <k6859386@gmail.com>
2026-09-09 21:55:36 +0530

    Create Day4_prime_analyzer.py

---
+++ Day4_prime_analyzer.py
@@ -0,0 +1,56 @@
+def findprime(n):
+    if n<=1:
+        return False
+    
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
+def factors(n):
+    fact=[]
+    for i in range(2,num):
+        if num%i==0:
+            fact.append(i)
+
+    return fact
+
+def primes_in_range(start,end):
+    plst=[]
+    for i in range(start,end+1):
+        if findprime(num):
+            plst.append(num)
+
+    return plst
+
+#main loop
+while True:
+    print("="*35)
+    print("     MATHS MACHINE       ")
+    print("="*35)
+    print("1)Check if a number is prime or not")
+    print("2)Check how many factors a number have")
+    print("3)Get list of all prime numbers in a specific range")
+    print("4)exit")
+    print("="*35)
+    input("Press enter to continue...")
+
+    choice=int(input("Enter your choice[1,2,3,4]: "))
+
+    if choice==1:
+        num=int(input("Enter a number to check to prime or not: "))
+        if findprime(num):
+            print(f"==>the {num} is prime")
+        else:
+            print(f"==>the {num} is not prime") 
+        input("\nPress Enter to continue...")
+    elif choice==2:
+        num=int(input("Enter the number to check how many factors it have: "))
+        factor=factors(num)
+        print(f"Factors of {num} is: {factor}")
+        print(f"Number of factors of {num} is:{len(factor)}")
+        input("\nPress Enter to continue...")
+
+    elif choice==3:
+        num=int(input())
+
