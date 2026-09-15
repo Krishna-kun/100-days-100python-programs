b7b5728f4c6db18b27b3a57bc9d7fb7b0d9444ed
Krishna <k6859386@gmail.com>
2026-09-15 23:13:54 +0530

    Create Day8_List_analyzer.py

---
+++ Day8_List_analyzer.py
@@ -0,0 +1,25 @@
+def basic_stat(number):
+    print("\n--->Basic statistics and extremes<---\n")
+
+
+
+
+
+
+
+
+
+
+
+
+#main loop
+while True:
+    print("="*35)
+    print("     LIST ANALYZER       ")
+    print("="*35)
+    print("1) Basic statistics and extremes")
+    print("2) Remove duplicate & sorts")
+    print("3) Filter Even 0dd & primes")
+    print("4) Slicing & list comprehensions")
+    print("5) Exit\n")
+    
