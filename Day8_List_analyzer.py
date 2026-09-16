ed7e8f2da27614a650bc0d7d00c2a293c59291e2
Krishna <k6859386@gmail.com>
2026-09-16 23:57:25 +0530

    Update Day8_List_analyzer.py

--- Day8_List_analyzer.py
+++ Day8_List_analyzer.py
@@ -81,4 +81,7 @@ while True:
     except ValueError:
         print("[!] Enter valid number...")
 
-    if 
+    if choose in [1,2,3,4]:
+        inputlst=int(input("\nEnter numbers separated by spaces (e.g. 12 5 7 12 8 19):"))
+        for num in inputlst:
+            
